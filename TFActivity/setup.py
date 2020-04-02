238d1c2a5efcd21ef159b3a1f40c42198f4cee7b
Claudia Skok Gibbs <cskokgibbs@flatironinstitute.org>
2020-04-02 14:24:44 -0400

    adding setup info

--- TFActivity/setup.py
+++ TFActivity/setup.py
@@ -1,14 +1,20 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
-    name='TFActivity',
+    name="TFActivity",
     version='0.1.0',
-    author='Claudia Skok Gibbs',
-    author_email='cskokgibbs@flatironinstitute.org',
-    packages=['TFActivity', 'TFActivity.test'],
-    #scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
-    #url='http://pypi.python.org/pypi/TowelStuff/',
-    license='LICENSE.txt',
-    description='Transcription Factor Activity Calculations',
-    long_description=open('README.txt').read(),
+    description="TFActivity: estimating transcription factor activity with local network component analysis",
+    long_description=open('README.md').read(),
+    url="https://github.com/cskokgibbs/TFActivity",
+    author="Claudia Skok Gibbs",
+    author_email="cskokgibbs@flatironinstitute.org",
+    maintainer="Claudia Skok Gibbs",
+    maintainer_email="cskokgibbs@flatironinstitute.org",
+    packages=find_packages(include=["TFActivity", 'TFActivity.test']),
+    zip_safe=False
 )
