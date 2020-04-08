ee45da52f5508edb3dec855d98ec4794dce288a6
Claudia Skok Gibbs <cskokgibbs@flatironinstitute.org>
2020-04-08 12:47:12 -0400

    adding citations

--- TFActivity/tfa.py
+++ TFActivity/tfa.py
@@ -3,7 +3,9 @@ import pandas as pd
 from sklearn.neighbors import NearestNeighbors
 
 """
-Local Network Component Analysis adapted from Shi et al 
+Code adapted from 
+Shi Q, Zhang C, Guo W, Zeng T, Lu L, Jiang Z, et al. 
+Local network component analysis for quantifying transcription factor activities. Methods. 2017;124: 25–35 
 https://www.sciencedirect.com/science/article/pii/S1046202317300506?via=ihub
 
 """
