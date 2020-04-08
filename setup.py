8e38cb5ea9e9215ffa79873c40161bdf40a7c3e8
Claudia Skok Gibbs <cskokgibbs@flatironinstitute.org>
2020-04-08 12:13:18 -0400

    adding requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
 from os import path
+from io import open
+
+# require python 3 and packages
+
 
 here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
@@ -9,6 +13,7 @@ setup(
     name="TFActivity",
     version='0.1.0',
     description="TFActivity: estimating transcription factor activity with local network component analysis",
+    install_requires=['python >= 3.7', 'numpy >= 1.18.1', 'pandas >= 1.0.1', 'sklearn >= 0.22.1'],
     long_description=open('README.md').read(),
     url="https://github.com/cskokgibbs/TFActivity",
     author="Claudia Skok Gibbs",
